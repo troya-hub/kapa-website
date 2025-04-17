2e8f1d1ca44a7eca918b2fc78be04271adfd4d6e
Luis <troyaluis56@gmail.com>
2025-04-16 22:16:55 -0500

    feat: add alias to @

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
 
 import tailwindcss from "@tailwindcss/vite";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "src"),
+			},
+		},
 	},
 });
