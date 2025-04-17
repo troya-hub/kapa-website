1dec0dc224ab46b4db3f9182b2a8473cab993990
Luis <troyaluis56@gmail.com>
2025-04-17 11:37:19 -0500

    feat: add changes to deploy to cloudflare pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,20 @@ import { fileURLToPath } from "url";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()],
-		resolve: {
-			alias: {
-				"@": path.resolve(__dirname, "src"),
-			},
-		},
+  vite: {
+      plugins: [tailwindcss()],
+      resolve: {
+          alias: {
+              "@": path.resolve(__dirname, "src"),
+          },
+      },
 	},
+
+  adapter: cloudflare(),
 });
