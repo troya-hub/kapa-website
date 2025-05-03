c17ed57e3ffd7c090c090be02dad550fd63e950b
Luis <troyaluis56@gmail.com>
2025-05-02 20:08:43 -0500

    feat: organize files

---
+++ src/data/designers.ts
@@ -0,0 +1,42 @@
+export const designers = [
+	{
+		name: "Jason Smith",
+		role: "Art Director",
+		image: JasonSmithImage,
+		score: {
+			value: "4,9",
+			background: "bg-[#DAE6FF]",
+			textColor: "text-[#3C7DFF]",
+		},
+	},
+	{
+		name: "Kimberly Doe",
+		role: "Illustrator",
+		image: KimberlyDoeImage,
+		score: {
+			value: "4,5",
+			background: "bg-[#FFE6D5]",
+			textColor: "text-[#F5690F]",
+		},
+	},
+	{
+		name: "Nick Jonas",
+		role: "Project Manager",
+		image: NickJonasImage,
+		score: {
+			value: "4,7",
+			background: "bg-[#D6FFF4]",
+			textColor: "text-[#12B288]",
+		},
+	},
+	{
+		name: "Matew Lee",
+		role: "Designer",
+		image: MatewLeeImage,
+		score: {
+			value: "4,5",
+			background: "bg-[#E7D9FF]",
+			textColor: "text-[#7D35C9]",
+		},
+	},
+];
