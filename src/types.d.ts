4d61e2b7f8edb4b77d9afa773e098b8853e32c8a
Luis <troyaluis56@gmail.com>
2025-06-11 11:40:17 -0500

    feat: add category samples

--- src/types.d.ts
+++ src/types.d.ts
@@ -19,4 +19,31 @@ type IBenefits = Array<{
 	darkIcon: AstroComponentFactory;
 }>;
 
+type Attachment = {
+	id: number;
+	public_path: string;
+	file_id: string;
+	name: name;
+	original_name: string;
+	instance_of: string;
+};
+
+type Samples = {
+	id: number;
+	task_category_id: number;
+	attachment: Array<Attachment>;
+};
+
+type TaskCategory = {
+	id: number;
+	title: string;
+	description: string;
+	slug: string;
+	image_url: string;
+	background: string;
+	enabled: boolean;
+	sort_order_on_samples: number;
+	samples: Array<Samples>;
+};
+
 export type IServiceThemeType = keyof typeof serviceThemes;
