a0038e9a770a874549c2b680218c26fa0e275e45
Luis <troyaluis56@gmail.com>
2025-06-13 22:39:18 -0500

    feat: add code and dev case study page

---
+++ src/data/case-studies.ts
@@ -0,0 +1,39 @@
+import CodeDevLogo from "@/assets/images/case-studies/code-dev-logo.webp?url";
+import CodeDevCover from "@/assets/images/case-studies/code-dev-cover.webp?url";
+import UpSolarLogo from "@/assets/images/case-studies/upsolar-logo.webp?url";
+import UpSolarCover from "@/assets/images/case-studies/upsolar-cover.webp?url";
+import SweetFishLogo from "@/assets/images/case-studies/sweet-fish-logo.webp?url";
+import SweetFishCover from "@/assets/images/case-studies/sweet-fish-cover.webp?url";
+
+export const caseStudies = [
+	{
+		slug: "code-and-dev",
+		title: "Marketing agency in Miami gets 10x output from 1 hour of work",
+		excerpt:
+			"Code & Dev is a business development company that works shoulder-by-shoulder with business owners, managers, and employees to accomplish marketing and sales goals.",
+		tags: ["Design", "Marketing", "Social Media"],
+		logo: CodeDevLogo,
+		image: CodeDevCover,
+		url: "/case-studies/code-and-dev",
+	},
+	{
+		slug: "upsolar",
+		title: "How a multinational corporation delegates their graphic design work in 10 minutes",
+		excerpt:
+			"Upsolar, a multinational corporation, delegates graphic design in 10 minutes for marketing campaigns across the world.",
+		tags: ["Design", "Marketing", "Social Media"],
+		logo: UpSolarLogo,
+		image: UpSolarCover,
+		url: "/case-studies/upsolar",
+	},
+	{
+		slug: "sweet-fish-media",
+		title: "Learn how the world top B2B podcast producer saves 100 hours and $45,000 per year",
+		excerpt:
+			"Sweet Fish Media, is the world-leading podcast agency the world, and is getting a lot of value from Kapa99 and is using the graphic design subscription in unexpected ways.",
+		tags: ["Design", "Marketing", "Social Media"],
+		logo: SweetFishLogo,
+		image: SweetFishCover,
+		url: "/case-studies/sweet-fish-media",
+	},
+];
