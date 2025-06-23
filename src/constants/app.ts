ad4d2018a7f181ad830c5fdc00dcf877eb48a398
Luis <troyaluis56@gmail.com>
2025-06-23 12:43:30 -0500

    feat: add pricing, discount and stripe code

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,2 +1,7 @@
 export const REGISTER_URL = "https://app.kapa99.com/register?coupon=300savings";
 export const LOGIN_URL = "https://app.kapa99.com/login";
+
+export const getRegisterUrlWithParams = (params: Record<string, string>) => {
+	const queryString = new URLSearchParams(params).toString();
+	return `${REGISTER_URL}?${queryString}`;
+};
